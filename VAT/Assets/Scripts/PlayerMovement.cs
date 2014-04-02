ea726e5ea1e60d3f3f8448d784b49f54b987c82f
Kevin Novak <novak009@cougars.csusm.edu>
2014-04-01 22:51:56 -0700

    Iso camera, movement, shooting, walking up slopes
    Yee buddy you better believe it.
    -Isometric camera, follows the player at a set distance
    -Player movement. WASD. There is an option (in the code) to use WASD and
    mouselook, but that's a bit weird.
    -Collision. Player collides with obstructions and the ground. He can go
    up some slanted objects, up to like 45 degrees it seems.
    -Shooting. He shoots a gun, it plays a noise and shows a flash of light.
    There's a public var (GunType) you can change. Auto means you can hold
    click to fire, otherwise, just 1 click = 1 shot.
    
    Assets>Scenes>Sandbox is the scene to load.
    I've been following this pretty strictly:
    https://www.youtube.com/playlist?list=PLFt_AvWsXl0ct75VmGXskJnzrStEbPD6d

---
+++ VAT/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+public class PlayerMovement : MonoBehaviour {
+
+
+	//Handling
+	public float rotationSpeed = 450;
+	public float walkSpeed = 5;
+	public float runSpeed = 8;
+
+	//State
+	private Quaternion targetRotation;
+
+	//Components
+	private CharacterController controller;
+
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 input = new Vector3(Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+
+		if (input != Vector3.zero)
+		{
+			targetRotation = Quaternion.LookRotation (input);
+			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle (transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+		}
+
+		Vector3 motion = input;
+		motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1)? .7f : 1;
+		motion *= (Input.GetButton ("Run")) ? runSpeed : walkSpeed;
+		motion += Vector3.up * -8;
+
+		controller.Move(motion * Time.deltaTime);
+	}
+}
